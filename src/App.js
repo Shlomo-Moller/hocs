4ebe50cc7b484890b03b107dae07db8704a40222
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-02 19:23:32 +0200

    Use BlogPost in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import UsersList from './components/UsersList'
+import BlogPost from './components/BlogPost'
+
+const POST_ID = 1
 
 const App = () => {
   return (
     <div className='App'>
       <h1>React HOCs</h1>
       <button id='change-simulator'>Simulate DS change</button>
+      <h2>UsersList</h2>
       <UsersList />
+      <h2>BlogPost #{POST_ID}</h2>
+      <BlogPost postId={POST_ID} />
     </div>
   )
 }
