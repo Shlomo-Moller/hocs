2cab887cfc13b4a3fe1017bc8b1b1fb0851da929
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-02 19:22:17 +0200

    Update DS. Add ti it getPost

--- src/ds.js
+++ src/ds.js
@@ -1,5 +1,7 @@
 const API = 'https://jsonplaceholder.typicode.com/'
 
+const getChangeButton = () => document.getElementById('change-simulator')
+
 const simulateChange = onChange => () => {
 	alert('Data source was "changed" (not really)')
 	onChange()
@@ -7,8 +9,9 @@ const simulateChange = onChange => () => {
 
 const DS = {
 	getUsers: () => fetch(`${API}users`).then(res => res.json()),
-	addChangeListener: onChange => document.getElementById('change-simulator').addEventListener('click', simulateChange(onChange)),
-	removeChangeListener: onChange => document.getElementById('change-simulator').removeEventListener('click', simulateChange(onChange)),
+	getPost: postId => fetch(`${API}posts/${postId}`).then(res => res.json()),
+	addChangeListener: onChange => getChangeButton().addEventListener('click', simulateChange(onChange)),
+	removeChangeListener: onChange => getChangeButton().removeEventListener('click', simulateChange(onChange)),
 }
 
 export default DS
