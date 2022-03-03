e6b140816e29c9552a95c3fc7306219b8d807d44
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-03 13:09:27 +0200

    Make BlogPost use the prop name configuration

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,14 +1,14 @@
 import withSubscription from './../withSubscription'
 
-const BlogPost = ({ data }) => {
+const BlogPost = ({ post }) => {
 	return (
 		<div className='BlogPost'>
-			<h6>{data?.title}</h6>
-			<pre>{data?.body}</pre>
+			<h6>{post?.title}</h6>
+			<pre>{post?.body}</pre>
 		</div>
 	)
 }
 
 const postFetcher = (DS, { postId }) => DS.getPost(postId)
 
-export default withSubscription(BlogPost, postFetcher)
+export default withSubscription(BlogPost, postFetcher, 'post')
