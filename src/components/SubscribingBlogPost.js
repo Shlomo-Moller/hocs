4f44ab856b8a8fad2acd484567a60cb960bc5bc8
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-03 10:20:06 +0200

    Fix name of SubscribingUsersList.js's component & format spaces

--- src/components/SubscribingBlogPost.js
+++ src/components/SubscribingBlogPost.js
@@ -4,30 +4,30 @@ import DS from '../ds'
 /**
  * Independently subscribes to the data source
  */
-const BlogPost = ({ postId }) => {
+const SubscribingBlogPost = ({ postId }) => {
 
 	const [post, setPost] = useState(null)
 
 	const onChange = useCallback(() => {
-    console.log('Fetching post...')
-    DS.getPost(postId).then(data => {
-      console.log('Got post! Updating state...')
-      setPost(data)
-    })
-  })
+		console.log('Fetching post...')
+		DS.getPost(postId).then(data => {
+			console.log('Got post! Updating state...')
+			setPost(data)
+		})
+	})
 
 	useEffect(() => {
-    onChange()
+		onChange()
 		DS.addChangeListener(onChange)
 		return () => DS.removeChangeListener(onChange)
 	}, [])
 
 	return (
-		<div className='BlogPost'>
+		<div className='SubscribingBlogPost'>
 			<h6>{post?.title}</h6>
 			<pre>{post?.body}</pre>
 		</div>
 	)
 }
 
-export default BlogPost
+export default SubscribingBlogPost
